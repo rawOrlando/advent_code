8f2bd59ffdfd7b7603e09d073674378206163705
Ryan Orlando <rynorlando@gmail.com>
2022-01-11 17:06:45 -0500

    Day 2 puzzles

---
+++ day_2.py
@@ -0,0 +1,24 @@
+
+
+current_depth = 0
+current_h_pos = 0
+aim = 0
+
+with open('day_2_puzzle_input.txt') as f:
+    lines = f.readlines()
+    for line in lines:
+        # fowards
+        if "fo" == line[0:2]:
+            current_h_pos += int(line[8:])
+            current_depth += (int(line[8:]) * aim)
+        # down
+        elif "do" == line[0:2]:
+            aim += int(line[5:])
+
+        elif "up" == line[0:2]:
+            aim -= int(line[3:])
+
+print(current_h_pos)
+print(current_depth)
+print(current_h_pos*current_depth)
+
